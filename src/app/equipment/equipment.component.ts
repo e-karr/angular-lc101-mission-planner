505e28fd064eb767a78505ed2b781b92237ce0e2
Elizabeth Karr <e.karr93@icloud.com>
2023-05-09 13:55:02 -0500

    allow user to add equipment

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = ['Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks'];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(item: string) {
+    let addMessage: string = "";
+
+    if (this.equipment.includes(item)) {
+      addMessage = "This item is already on your rocket.";
+    } else if (item === "") {
+      addMessage = "Please enter an item.";
+    } else {
+      this.equipment.push(item);
+    }
+
+    return addMessage;
   }
 
 }
