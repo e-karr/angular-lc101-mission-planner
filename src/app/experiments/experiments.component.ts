449240fa31321c3b658b219b48b249cdb2e66d27
Elizabeth Karr <e.karr93@icloud.com>
2023-05-09 14:14:02 -0500

    allow user to add experiments

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
+  experiments: string[] = ['Mars soil sample', 'Plant growth in habitat', 'Human bone density'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(experiment: string) {
+    let addMessage: string = "";
+
+    if (this.experiments.includes(experiment)) {
+      addMessage = 'Already in experiments list';
+    } else if (experiment === "") {
+      addMessage = 'Please enter an experiment';
+    } else {
+      this.experiments.push(experiment);
+    }
+
+    return addMessage;
   }
 
 }
