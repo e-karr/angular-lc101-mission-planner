a1599ac20339f76df2074b2a2f486af3cd18843a
Elizabeth Karr <e.karr93@icloud.com>
2023-05-10 18:28:33 -0500

    apply prettier to html files

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,19 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  missionName: string = "Mars 2030";
-  rocketName: string = "Plasma Max";
+  missionName: string = 'Mars 2030';
+  rocketName: string = 'Plasma Max';
   editingMission: boolean = false;
   editingRocket: boolean = false;
-  
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor() {}
+
+  ngOnInit() {}
 
   updateMission(updatedName: string) {
     this.missionName = updatedName;
@@ -26,5 +24,4 @@ export class HeaderComponent implements OnInit {
     this.rocketName = updatedName;
     this.editingRocket = false;
   }
-
 }
