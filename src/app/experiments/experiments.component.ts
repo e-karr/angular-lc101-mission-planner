9e6cde740f7b064737956babfd40e75a24bea566
Elizabeth Karr <e.karr93@icloud.com>
2023-05-16 16:23:05 -0500

    create code to select experiments

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experiments',
   templateUrl: './experiments.component.html',
-  styleUrls: ['./experiments.component.css']
+  styleUrls: ['./experiments.component.css'],
 })
 export class ExperimentsComponent implements OnInit {
-   experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density changes', 'Water droplet podcast for grades K-5', 'Satellite launch'];
+  experiments: string[] = [
+    'Moon soil sample',
+    'Plant growth in orbit',
+    'Human bone density changes',
+    'Water droplet podcast for grades K-5',
+    'Satellite launch',
+  ];
 
-   constructor() { }
+  selectedExperiments: string[] = [];
 
-   ngOnInit() { }
+  constructor() {}
 
+  ngOnInit() {}
+
+  selectExperiment(experiment: string) {
+    if (
+      !this.selectedExperiments.includes(experiment) &&
+      this.selectedExperiments.length < 2
+    ) {
+      this.selectedExperiments.push(experiment);
+    }
+  }
 }
