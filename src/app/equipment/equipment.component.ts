81285fabf71186358c07f271ecf6636471a0c44f
Elizabeth Karr <e.karr93@icloud.com>
2023-05-17 18:28:13 -0500

    create equipment interface

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+interface equipment {
+  name: string;
+  mass: number;
+}
+
 @Component({
   selector: 'app-equipment',
   templateUrl: './equipment.component.html',
   styleUrls: ['./equipment.component.css'],
 })
 export class EquipmentComponent implements OnInit {
-  equipmentItems: object[] = [
+  equipmentItems: equipment[] = [
     { name: 'Duct Tape', mass: 0.5 },
     { name: 'Space Camera', mass: 20 },
     { name: 'Food', mass: 150 },
@@ -17,7 +22,7 @@ export class EquipmentComponent implements OnInit {
     { name: 'Satellite', mass: 1200 },
     { name: 'R2 Unit', mass: 32 },
   ];
-  cargoHold: object[] = [];
+  cargoHold: equipment[] = [];
   cargoMass: number = 0;
   maximumAllowedMass: number = 2000;
   maxItems: number = 10;
@@ -27,12 +32,12 @@ export class EquipmentComponent implements OnInit {
   ngOnInit() {}
 
   // Code your addItem function here:
-  addItem(equipment: object) {
+  addItem(equipment: equipment) {
     let counter: number = this.countItems(equipment);
 
     if (counter < 2) {
       this.cargoHold.push(equipment);
-      this.cargoMass += equipment['mass'];
+      this.cargoMass += equipment.mass;
     }
 
     if (this.cargoMass >= this.maximumAllowedMass - 200) {
@@ -42,7 +47,7 @@ export class EquipmentComponent implements OnInit {
     return false;
   }
 
-  countItems(equipment: object): number {
+  countItems(equipment: equipment): number {
     let counter: number = 0;
     for (let item of this.cargoHold) {
       if (item === equipment) {
@@ -53,9 +58,9 @@ export class EquipmentComponent implements OnInit {
     return counter;
   }
 
-  removeItem(item: object) {
+  removeItem(item: equipment) {
     let index = this.cargoHold.indexOf(item);
-    this.cargoMass -= item['mass'];
+    this.cargoMass -= item.mass;
     this.cargoHold.splice(index, 1);
   }
 
